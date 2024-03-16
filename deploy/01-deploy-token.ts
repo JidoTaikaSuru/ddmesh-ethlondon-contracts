0ca863e4e0da721d90e0b91e29c71ad240f67b7c
vantwoutmaarten <maarten@vantwout.net>
2024-03-16 10:21:05 +0000

    fix: create the hardhat project add the token contract and the token deployment file.

---
+++ deploy/01-deploy-token.ts
@@ -0,0 +1,38 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import verify from "../utils/verify";
+import { developmentChains } from "../helper-hardhat-config";
+
+const deployToken: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  console.log("Token deployed by:", deployer);
+  const token = await deploy("Token", {
+    from: deployer,
+    // args: [],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    // waitConfirmations: networkConfig[chainId]?.blockConfirmations || 0,
+  });
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(token.address, "contracts/Token.sol:Token", []);
+  }
+  console.log("Token deployed to:", token.address);
+  console.log("Token deployed on network:", network.name);
+  const Token = await hre.ethers.getContractFactory("Token");
+  const tokenContract = Token.attach(token.address);
+  const txRole = await tokenContract.grantRole(
+    await tokenContract.MINTER_ROLE(),
+    deployer
+  );
+  await txRole.wait();
+};
+
+export default deployToken;
+deployToken.tags = ["all", "token"];
